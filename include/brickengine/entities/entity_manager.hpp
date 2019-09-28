236d6559de3fedb7dac8e12fe9a25710ebd1d3c3
Mark <mmeer8@avans.nl>
2019-09-28 12:42:47 +0200

    Some minor changes
    Including the methods and a change in how the unique_ptr's are build.

--- include/brickengine/entities/entity_manager.hpp
+++ include/brickengine/entities/entity_manager.hpp
@@ -15,17 +15,19 @@ class EntityManager{
         ~EntityManager();
 
         int createEntity(const std::vector<Component> &components);
-        void removeEntity();
+        void removeEntity(const int entityId);
         
         const Component getComponent(const int entityId);
 
         void addComponentToEntity(const int entityId, const Component component); 
         void removeComponentFromEntity(const int entityId);
 
+        void printEntities();
+
     private:
-        std::vector<std::unique_ptr<int>> entities;
+        std::unique_ptr<std::vector<int>> entities;
         std::unique_ptr<int> lowest_unassigned_entity_id { new int(-1) };
-        std::unordered_map<std::string, std::unique_ptr<Component>> components_by_class;
+        std::unique_ptr<std::unordered_map<std::string, std::vector<Component>>> components_by_class;
 };
 
 #endif /* FILE_ENTITY_MANAGER_HPP */
--- lib/entities/entity_manager.cpp
+++ lib/entities/entity_manager.cpp
@@ -9,23 +9,23 @@
 
 EntityManager::EntityManager(){
     
-    // entities.push_back(std::unique_ptr<int>{new int(5)});
 
-    // for(std::unique_ptr<int>& e : entities){
-    //     std::cout << *e << '\n';
-    // }
-
-    // components_by_class["Jan"] = std::unique_ptr<Component>{new Component()};
 
     // for(auto const& map: components_by_class){
     //     std::cout << "key: " + map.first << ", " << map.second << "\n";
     // }
+    
+    // std::unique_ptr<std::vector<Component>> componentList;
+    // componentList->push_back(new Component());
+    // createEntity(componentList);
 
-    addComponentToEntity(1, Component());
+    //addComponentToEntity(1, Component());
+    
+    printEntities();
 }
 
 int EntityManager::createEntity(const std::vector<Component> &components){
-    entities.push_back(std::unique_ptr<int>{new int(++*lowest_unassigned_entity_id)});
+    entities->push_back(++*lowest_unassigned_entity_id);
 
     for(auto const c : components){
         addComponentToEntity(*lowest_unassigned_entity_id, c);
@@ -35,12 +35,34 @@ int EntityManager::createEntity(const std::vector<Component> &components){
 }
 
 void EntityManager::addComponentToEntity(const int entityId, const Component component){
-    std::unique_ptr<std::string> componentType {new std::string(typeid(component).name())};
+    std::string componentType {std::string(typeid(component).name())};
 
-    std::cout << "entityId is: " << typeid(int).name() << '\n';
-    std::cout << "component is: " << typeid(component).name() << '\n';
+    if(components_by_class->count(componentType) == 0){
+        components_by_class->insert(std::make_pair(componentType, std::vector<Component>()));
+    }
+    
+    std::vector<Component> componentsPerId = components_by_class->at(componentType);
+    componentsPerId[entityId] = component;
+}
+
+void EntityManager::removeComponentFromEntity(const int entityId){
+    
+}
+
+const Component EntityManager::getComponent(const int entityId){
+    
+}
+
+void EntityManager::removeEntity(const int entityId){
+    entities->erase(entities->begin() + entityId);
+}
+
+void EntityManager::printEntities(){
+    for(std::unique_ptr<int>& e : entities){
+        std::cout << *e << '\n';
+    }
 }
 
 EntityManager::~EntityManager(){
-    
+
 }
