1e5e333abff93bd9b9e76a57d0df1f3704159fbd
JanSchollaert <jbscholl@avans.nl>
2019-09-30 12:09:23 +0200

    Fixes

--- lib/entity_factory.cpp
+++ lib/entity_factory.cpp
@@ -3,10 +3,6 @@
 #include "brickengine/components/component.hpp"
 #include "brickengine/entities/entity_manager.hpp"
 
-EntityFactory::EntityFactory(std::shared_ptr<EntityManager> _entityManager){
-    entityManager = std::make_shared<EntityManager>(&_entityManager);
-}
-
 int EntityFactory::createDefault(){
     return entityManager->createEntity(
         std::vector<Component>{Component{}}
