70a42b24a3c1bec61a51b04e920394d456296b9b
JanSchollaert <jbscholl@avans.nl>
2019-09-30 14:45:40 +0200

    Fixes

--- include/brickengine/entities/entity_manager.hpp
+++ include/brickengine/entities/entity_manager.hpp
@@ -19,15 +19,37 @@ class EntityManager{
         void removeEntity(const int entityId);
 
         template <class T>
-        std::unique_ptr<std::vector<std::unique_ptr<EntityWithComponent<T>>>> getAllEntities();
+        std::unique_ptr<std::vector<std::unique_ptr<EntityWithComponent<T>>>> getAllEntities(){
+            std::string componentType {std::string(typeid(T).name())};
+            auto list = std::unique_ptr<std::vector<std::unique_ptr<EntityWithComponent<T>>>>(new std::vector<std::unique_ptr<EntityWithComponent<T>>>(components_by_class->at(componentType).size()));
+            list.get()->clear();
+            if(components_by_class->count(componentType) > 0){
+                for(auto const& obj : components_by_class->at(componentType)){
+                    std::unique_ptr<EntityWithComponent<T>> ewc (new EntityWithComponent<T>(obj.first, obj.second.get()));
+                    list.get()->push_back(std::move(ewc));
+                }
+            }
+            return list;
+        }
         
         template <class T>
-        T* getComponent(const int entityId) const;
+        void removeComponentFromEntity(const int entityId){    
+            std::string componentType {std::string(typeid(T).name())};
 
-        void addComponentToEntity(const int entityId, const Component component); 
+            components_by_class->at(componentType).erase(entityId);
+        }
 
         template <class T>
-        void removeComponentFromEntity(const int entityId);
+        T* getComponent(const int entityId) const{
+            std::string componentType {std::string(typeid(T).name())};
+            if(components_by_class->count(componentType) > 0){
+                return (T*) components_by_class->at(componentType).at(entityId).get();
+            }else{
+                return (T*) nullptr;
+            }
+        }
+
+        void addComponentToEntity(const int entityId, const Component component); 
 
     private:
         std::unique_ptr<std::vector<int>> entities;
