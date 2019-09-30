6928c934a3358dd22cd484cc44f769a329c278c8
Peter-Jan Gootzen <16726988+Peter-JanGootzen@users.noreply.github.com>
2019-09-30 12:18:24 +0200

    Renderer (excl animations) (#3)
    * This is unstable and has a weird error (y)
    
    * Render is gaining steam
    
    Co-authored-by: Luuk Santegoeds <lsantego@avans.nl>
    
    * Added renderable_factory implementation
    
    Co-authored-by: Luuk Santegoeds <lsantego@avans.nl>
    
    * Implementing renderables polymorphism
    
    Co-authored-by: Luuk Santegoeds <lsantego@avans.nl>
    
    * WIP commit
    
    * Drawing Verstappen works
    
    Co-authored-by: Luuk Santegoeds <lsantego@avans.nl>
    
    * Renderer done and leak tested
    
    Co-authored-by: Luuk Santegoeds <lsantego@avans.nl>
    
    * Fixed typos and improved init error handling
    
    * Forgot to save
    
    * Fixed Jans nit picky feedback >:)
    
    * Added some comments

--- include/brickengine/rendering/renderer.hpp
+++ include/brickengine/rendering/renderer.hpp
@@ -2,16 +2,27 @@
 #define FILE_RENDERER_HPP
 
 #include <functional>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <memory>
 
-#include "brickengine/rendering/renderables/image.hpp"
+#include "SDL2/SDL.h"
+#include "brickengine/rendering/renderables/renderable.hpp"
 
 class Renderer {
 public:
-    void ClearScreen();
-    void DrawScreen();
-    void Render(Renderable renderable);
-    void Render(Image renderable);
-    void Render();
+    Renderer(std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)> r, std::vector<int> layers);
+    void clearScreen();
+    void drawScreen();
+    void queueRenderable(Renderable& r);
+    SDL_Texture* createTextureFromSurface(SDL_Surface* surface) const;
+    void render();
+private:
+    std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)> sdl_renderer;
+    std::unique_ptr<std::unordered_map<int, Renderable&>> renderQueue;
+    void render(Renderable& renderable);
+    std::vector<int> layers;
 };
 
-#endif 
+#endif
