4603abb425e26f83afafa945c91dcedfa53a1c67
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-10-01 12:51:34 +0200

    Fixed more indenting

--- include/brickengine/entities/entity_factory.hpp
+++ include/brickengine/entities/entity_factory.hpp
@@ -6,12 +6,12 @@
 #include "brickengine/entities/entity_manager.hpp"
 
 class EntityFactory{
-    public:
-        EntityFactory(std::shared_ptr<EntityManager> _entityManager) : entityManager(_entityManager){
-        };
-        ~EntityFactory() = default;
-    private:
-        std::shared_ptr<EntityManager> entityManager;
+public:
+    EntityFactory(std::shared_ptr<EntityManager> _entityManager) : entityManager(_entityManager){
+    };
+    ~EntityFactory() = default;
+private:
+    std::shared_ptr<EntityManager> entityManager;
 };
 
 #endif /* FILE_ENTITY_FACTORY_HPP */
--- include/brickengine/systems/system.hpp
+++ include/brickengine/systems/system.hpp
@@ -4,14 +4,14 @@
 #include "brickengine/entities/entity_manager.hpp"
 
 class System{
-    public:
-        System(std::shared_ptr<EntityManager> _entityManager) : entityManager(_entityManager) {
+public:
+    System(std::shared_ptr<EntityManager> _entityManager) : entityManager(_entityManager) {
 
-        };
-        virtual ~System();
-        virtual void update(double deltaTime) = 0;
-    protected:
-        std::shared_ptr<EntityManager> entityManager;
+    };
+    virtual ~System();
+    virtual void update(double deltaTime) = 0;
+protected:
+    std::shared_ptr<EntityManager> entityManager;
 };
 
 #endif /* FILE_SYSTEM_HPP */
