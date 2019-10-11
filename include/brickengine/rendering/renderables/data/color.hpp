1ac14de44c81e74b27af5a891f30fce60a86abf6
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-10-11 22:57:09 +0200

    Added color compare operators

--- include/brickengine/rendering/renderables/data/color.hpp
+++ include/brickengine/rendering/renderables/data/color.hpp
@@ -6,5 +6,13 @@ struct Color
     unsigned short int g;
     unsigned short int b;
     unsigned short int a;
+    bool operator==(const Color& other)
+    {
+        return r == other.r && g == other.g && b == other.b && a == other.a;
+    }
+    bool operator!=(const Color& other)
+    {
+        return r == other.r || g == other.g || b == other.b || a == other.a;
+    }
 };
 #endif
