37209b9734b3595d1be14726a53f883e9c888843
Luuk Santegoeds <santegoedsweb@gmail.com>
2019-11-04 21:50:01 +0100

    Added alpha

--- lib/rendering/renderable_factory.cpp
+++ lib/rendering/renderable_factory.cpp
@@ -14,8 +14,9 @@
 RenderableFactory::RenderableFactory(std::shared_ptr<Renderer> r) : renderer(r) {
 }
 
-std::unique_ptr<Texture> RenderableFactory::createImage(std::string path, int layer, std::unique_ptr<Rect> dst) const {
+std::unique_ptr<Texture> RenderableFactory::createImage(std::string path, int layer, std::unique_ptr<Rect> dst, int alpha) const {
     SDL_Texture* texture = this->createBMPTexture(path);
+    SDL_SetTextureAlphaMod(texture, alpha);
 
     return std::make_unique<Texture>(texture, layer, std::move(dst));
 }
