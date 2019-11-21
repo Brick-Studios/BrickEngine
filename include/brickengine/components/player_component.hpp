d70a5201d99587e38b1c75bb8805a28ec33b4e03
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-11-21 13:07:39 +0100

    Added player name

--- include/brickengine/components/player_component.hpp
+++ include/brickengine/components/player_component.hpp
@@ -2,15 +2,17 @@
 #define FILE_PLAYER_COMPONENT_HPP
 
 #include "brickengine/components/component_impl.hpp"
+#include  <string>
 
 class PlayerComponent : public ComponentImpl<PlayerComponent> {
 public:
-    PlayerComponent(int player_id, bool disabled = false);
+    PlayerComponent(int player_id, std::string name, bool disabled = false);
     static std::string getNameStatic();
 
     // Data
     int player_id;
     bool disabled;
+    std::string name;
 };
 
 #endif // FILE_PLAYER_COMPONENT_HPP
--- lib/components/player_component.cpp
+++ lib/components/player_component.cpp
@@ -1,7 +1,7 @@
 #include "brickengine/components/player_component.hpp"
 
-PlayerComponent::PlayerComponent(int player_id, bool disabled)
-    : player_id(player_id), disabled(disabled) {}
+PlayerComponent::PlayerComponent(int player_id, std::string name, bool disabled)
+    : player_id(player_id), disabled(disabled), name(name) {}
 
 std::string PlayerComponent::getNameStatic() {
     return "PlayerComponent";
