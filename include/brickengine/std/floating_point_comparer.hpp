7a184413a9d79f088ea2a05ef534930deb3dba33
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-12-06 22:32:05 +0100

    Almost fixed floating point inprecision
    Co-authored-by: Bram-Boris Meerlo <me@bramboris.nl>

---
+++ include/brickengine/std/floating_point_comparer.hpp
@@ -0,0 +1,19 @@
+#ifndef FILE_FLOAT_POINT_COMPARER_HPP
+#define FILE_FLOAT_POINT_COMPARER_HPP
+
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
+#include <algorithm>
+
+ 
+class FloatingPointComparer {
+public:
+    static bool is_equal_to_zero(double f) {
+        return f < 1e-10;
+    }
+};
+
+#endif // FILE_FLOATING_POINT_COMPARER_HPP
