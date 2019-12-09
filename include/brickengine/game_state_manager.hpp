6c82e7f719e4752a41178aeea2f530d785e44d4b
Mark <mmeer8@avans.nl>
2019-12-09 20:21:31 +0100

    Changes to game_state_manager V2.

--- include/brickengine/game_state_manager.hpp
+++ include/brickengine/game_state_manager.hpp
@@ -8,6 +8,7 @@
 #include "brickengine/systems/system.hpp"
 #include "brickengine/exceptions/reset_on_set_state_not_set.hpp"
 #include "brickengine/exceptions/state_systems_not_set.hpp"
+#include "brickengine/scenes/scene_reset_state.hpp"
 
 template<typename State>
 class GameStateManager {
@@ -15,25 +16,30 @@ public:
     using Systems = std::vector<std::unique_ptr<System>>;
     using StateSystems = std::unordered_map<State, std::unique_ptr<Systems>>;
 
-    GameStateManager(std::unique_ptr<StateSystems> state_systems, std::unordered_map<State, bool> reset_on_set_state, State begin_state)
+    GameStateManager(std::unique_ptr<StateSystems> state_systems, std::unordered_map<State, SceneResetState> reset_on_set_state, State begin_state)
         : state_systems(std::move(state_systems)), reset_on_set_state(reset_on_set_state), current_state(begin_state) {}
 
     void setState(State state) {
         bool reset_systems = true;
 
-        // The game is initialized and has a current_state.
-        if((int)current_state != 0){                
-            // Check whether the state exists in the reset on set state.
-            if (!reset_on_set_state.count(state))
-                throw ResetOnSetStateNotSetException<State>(state);
-            
-            // Checks whether the state has systems affiliated with it.
-            if (!state_systems->count(state))
-                throw StateSystemsNotSet<State>(state);
+        // Check whether the state exists in the reset on set state.
+        if (!reset_on_set_state.count(state))
+            throw ResetOnSetStateNotSetException<State>(state);
+        
+        // Checks whether the state has systems affiliated with it.
+        if (!state_systems->count(state))
+            throw StateSystemsNotSet<State>(state);
 
-            reset_systems = reset_on_set_state.at(state);
+        // The game is initialized and has a current_state.
+        if((int)current_state != 0){
+            // Check whether the current scene should be resetted on start.
+            if(!reset_on_set_state.at(current_state).reset_on_start ||
+            !reset_on_set_state.at(current_state).reset_on_end)
+                reset_systems = false;
         }
 
+        if(!reset_systems)
+            reset_systems = reset_on_set_state.at(state).reset_on_start;
 
         // Reset systems
         if (reset_systems) {
@@ -48,7 +54,7 @@ public:
     }
 private:
     std::unique_ptr<StateSystems> state_systems;
-    std::unordered_map<State, bool> reset_on_set_state;
+    std::unordered_map<State, SceneResetState> reset_on_set_state;
     State current_state;
 };
 
