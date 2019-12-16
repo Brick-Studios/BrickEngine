a5b5844ff7774ccc6eb23696026b2b72abd71258
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-12-16 11:17:38 +0100

    Added DirectionConverter

--- include/brickengine/enum/direction.hpp
+++ include/brickengine/enum/direction.hpp
@@ -2,8 +2,31 @@
 #define FILE_DIRECTION_HPP
 
 enum class Direction { 
-    POSITIVE, 
-    NEGATIVE
+    POSITIVE = 0, 
+    NEGATIVE = 1
+};
+
+#include <iostream>
+#include <exception>
+
+class UnknownDirectionException : public std::exception {
+private:
+  std::string m_msg;
+public:
+  UnknownDirectionException(int layer) : m_msg(std::string("The entered direction ") + std::to_string(layer) + std::string(" is not supported")){}
+
+  virtual const char* what() const throw() {
+    return m_msg.c_str();
+  }
+};
+
+class DirectionConverter {
+public:
+    static Direction convertInt(int i) {
+        if (i < 0 || i > 8)
+            throw UnknownDirectionException(i);
+        return static_cast<Direction>(i);
+    }
 };
 
 #endif //FILE_DIRECTION_HPP
